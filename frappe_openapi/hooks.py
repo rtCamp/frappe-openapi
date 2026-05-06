f03ff732c940f1938bae87b70507411b40716c98
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2026-05-06 15:39:56 +0530

    Rmeove version from hooks.py

--- frappe_openapi/hooks.py
+++ frappe_openapi/hooks.py
@@ -4,7 +4,6 @@ app_publisher = "rtCamp"
 app_description = "Frappe app to generate and visualize whitelisted APIs"
 app_email = "frappe@rtcamp.com"
 app_license = "agpl-3.0"
-app_version = "0.1.0"
 
 # Apps
 # ------------------
